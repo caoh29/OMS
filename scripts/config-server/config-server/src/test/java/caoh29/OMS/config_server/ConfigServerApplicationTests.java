c524f75fd9cfdfdda2d2cda427c32cf992a91061
Camilo Ordoñez <cronox20@gmail.com>
2025-07-25 14:20:11 -0400

    trying to configure spring cloud config

---
+++ scripts/config-server/config-server/src/test/java/caoh29/OMS/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package caoh29.OMS.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
