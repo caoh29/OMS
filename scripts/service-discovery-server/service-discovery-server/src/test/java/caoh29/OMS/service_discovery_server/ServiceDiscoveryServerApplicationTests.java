7b3f05d5ea604ab8e43dd9753393028bfeea790a
Camilo Ordoñez <cronox20@gmail.com>
2025-07-26 11:20:56 -0400

    integrating eureka with config server, properties are being passed in docker-compose file as environment variables

---
+++ scripts/service-discovery-server/service-discovery-server/src/test/java/caoh29/OMS/service_discovery_server/ServiceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package caoh29.OMS.service_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
