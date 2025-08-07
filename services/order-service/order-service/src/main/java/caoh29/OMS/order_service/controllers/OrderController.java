0dfffe15d88cd3fa2d1a720c297625b3c88a87d5
Camilo Ordoñez <cronox20@gmail.com>
2025-08-07 13:59:13 -0400

    create api-gateway and integrated with simple order-service, needs improvement on both ends but it works so far

--- services/order-service/order-service/src/main/java/caoh29/OMS/order_service/controllers/OrderController.java
+++ services/order-service/order-service/src/main/java/caoh29/OMS/order_service/controllers/OrderController.java
@@ -1,2 +1,22 @@
-package caoh29.OMS.order_service.controllers;public class OrderController {
+package caoh29.OMS.order_service.controllers;
+
+import caoh29.OMS.order_service.entities.Order;
+import caoh29.OMS.order_service.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping()
+    public List<Order> getOrders() {
+        return this.orderService.getOrders();
+    }
 }
--- services/order-service/order-service/src/main/java/caoh29/OMS/order_service/services/OrderService.java
+++ services/order-service/order-service/src/main/java/caoh29/OMS/order_service/services/OrderService.java
@@ -1,2 +1,29 @@
-package caoh29.OMS.order_service.services;public class OrderService {
+package caoh29.OMS.order_service.services;
+
+import caoh29.OMS.order_service.entities.Order;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrderService {
+    public List<Order> getOrders() {
+        ArrayList<Order> list = new ArrayList<>();
+        list.add(new Order(
+                "1",
+                2,
+                "John Doe",
+                "john@email.com",
+                "PENDING"
+        ));
+        list.add(new Order(
+                "2",
+                6,
+                "Daniel Smith",
+                "daniel@email.com",
+                "COMPLETED"
+        ));
+        return list;
+    }
 }
